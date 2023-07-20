9a91a2b6cd4838288f15e245e179dd4f30fd25bb
KhanhHungVo <khanhhungvo001@gmail.com>
2023-07-20 20:38:25 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo-list';
+  todos : Todo[] = [];
+  newTodo: string;
+
+  saveTodo(){
+    debugger
+    if(this.newTodo){
+      let todo = new Todo();
+      todo.description = this.newTodo;
+      todo.isCompleted = false;
+      this.todos.push(todo);
+      this.newTodo = '';
+    } else {
+      alert('Please enter todo');
+    }
+  }
+
+  done(id:number){
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  remove(id:number){
+    this.todos.splice(id,1);
+  }
 }
